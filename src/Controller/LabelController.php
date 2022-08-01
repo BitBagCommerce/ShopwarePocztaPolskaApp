e253c38ead9ff1d2044d36331401fc2f35780435
Norbert Glanc <norglanc@gmail.com>
2022-08-01 07:27:33 +0200

    Add datetime for shipping date in manifest, fix code after code review

--- src/Controller/LabelController.php
+++ src/Controller/LabelController.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace BitBag\ShopwarePocztaPolskaApp\Controller;
 
-use BitBag\ShopwareAppSystemBundle\Exception\ShopNotFoundException;
-use BitBag\ShopwareAppSystemBundle\Factory\Context\ContextFactoryInterface;
 use BitBag\ShopwareAppSystemBundle\Model\Action\ActionInterface;
 use BitBag\ShopwareAppSystemBundle\Model\Feedback\NewTab;
-use BitBag\ShopwareAppSystemBundle\Repository\ShopRepositoryInterface;
 use BitBag\ShopwareAppSystemBundle\Response\FeedbackResponse;
 use BitBag\ShopwareAppSystemBundle\Service\DocumentServiceInterface;
 use BitBag\ShopwarePocztaPolskaApp\Exception\Order\OrderDocumentNotFoundException;
 use BitBag\ShopwarePocztaPolskaApp\Factory\FeedbackResponseFactoryInterface;
 use BitBag\ShopwarePocztaPolskaApp\Finder\OrderFinderInterface;
-use BitBag\ShopwarePocztaPolskaApp\Validator\OrderValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Vin\ShopwareSdk\Data\Context;
 use Vin\ShopwareSdk\Data\Entity\Document\DocumentEntity;
@@ -27,11 +22,8 @@ final class LabelController extends AbstractController
 {
     public function __construct(
         private FeedbackResponseFactoryInterface $feedbackResponseFactory,
-        private ContextFactoryInterface $contextFactory,
-        private ShopRepositoryInterface $shopRepository,
         private DocumentServiceInterface $documentService,
-        private OrderFinderInterface $orderFinder,
-        private OrderValidatorInterface $orderValidator
+        private OrderFinderInterface $orderFinder
     ) {
     }
 
@@ -40,16 +32,14 @@ final class LabelController extends AbstractController
         $orderId = $action->getData()->getIds()[0] ?? '';
         $order = $this->orderFinder->getWithAssociations($orderId, $context);
 
-        $shopId = $action->getSource()->getShopId();
+        $shippingMethod = $order->deliveries?->first()?->shippingMethod ?? null;
+        if (null === $shippingMethod) {
+            return $this->feedbackResponseFactory->createError('bitbag.shopware_poczta_polska_app.order.shipping_method.not_found');
+        }
 
-        try {
-            $this->orderValidator->validate(
-                $shopId,
-                $order,
-                $context
-            );
-        } catch (\Exception $e) {
-            return $this->feedbackResponseFactory->createError($e->getMessage());
+        $technicalName = $shippingMethod->getTranslated()['customFields']['technical_name'] ?? null;
+        if (CreatePackageController::SHIPPING_KEY !== $technicalName) {
+            return $this->feedbackResponseFactory->createError('bitbag.shopware_poczta_polska_app.order.shipping_method.not_polish_post');
         }
 
         $redirectUrl = $this->generateUrl(
@@ -61,20 +51,9 @@ final class LabelController extends AbstractController
         return new FeedbackResponse(new NewTab($redirectUrl));
     }
 
-    public function showLabel(Request $request): Response
+    public function showLabel(Request $request, Context $context): Response
     {
         $data = $request->query->all();
-        $shopId = $data['shop-id'] ?? '';
-
-        $shop = $this->shopRepository->find($shopId);
-        if (null === $shop) {
-            throw new ShopNotFoundException($shopId);
-        }
-
-        $context = $this->contextFactory->create($shop);
-        if (null === $context) {
-            throw new UnauthorizedHttpException('');
-        }
 
         $orderId = $data['orderId'] ?? '';
         $order = $this->orderFinder->getWithAssociations($orderId, $context);
